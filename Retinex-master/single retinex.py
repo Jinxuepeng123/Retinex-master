059c932ceb14ac414e8ff87f59f5f120242057e6
jinxuepeng123 <1475981919@qq.com>
2021-05-09 21:19:39 +0800

    无

--- Retinex-master/single retinex.py
+++ Retinex-master/single retinex.py
@@ -15,17 +15,22 @@ if len(img_list) == 0:
 with open('config.json', 'r') as f:
     config = json.load(f)
 
+img_list.sort(key= lambda x:int(x[:-4]))   #排序得好方法
 for img_name in img_list:
     img = cv2.imread(os.path.join(data_path, img_name))
+
     HSV_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  #将V通道数据变为float64处理完后再将V变为unit8再再转换为图片形式，最后保存
     #创建副本以免V_channel与hsv图像公用内存
     V_channel = np.zeros_like(img[:,:,0])
     V_channel=HSV_img[:,:,2]   #V_channel赋值
     V_channel = np.float64(V_channel)    #转换成float 64，接下来对V空间进行处理，且赋值给一个中间量
-    temp=retinex.singleScaleRetinex(V_channel,80)   #有15，80，250三个选择，先选80
+    temp=retinex.singleScaleRetinex(V_channel,300)   #有15，80，250三个选择，先选80
+    #temp = temp * 100   #数值太小了将每个像素乘以10
     temp= np.uint8(temp)       #将temp转换到unit8,方便图像显示，接下来赋值给hsv的V
+
     HSV_img[:,:,2]=temp     #接下来将hsv转化到RGB
     RGB=cv2.cvtColor(HSV_img, cv2.COLOR_HSV2BGR)
+
     cv2.imshow('origin',img)
     cv2.imshow('refine',RGB)
     cv2.waitKey()
